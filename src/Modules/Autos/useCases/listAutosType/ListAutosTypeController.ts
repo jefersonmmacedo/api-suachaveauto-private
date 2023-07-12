9dff74b5c6f62a0920e08e851bda86aed31dc33f
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2023-07-12 16:25:43 -0300

    Limita type

--- src/Modules/Autos/useCases/listAutosType/ListAutosTypeController.ts
+++ src/Modules/Autos/useCases/listAutosType/ListAutosTypeController.ts
@@ -11,8 +11,10 @@ class ListAutosTypeController {
   async handle(req: Request, res: Response) {
     const {type} = req.params;
     const availability = "Disponível"
+    const page = 0;
+    const limit = 12
 
-   await collections.autos.find({availability, type}).sort( { created_at: -1 } ).toArray(function(err, result){
+   await collections.autos.find({availability, type}).sort( { created_at: -1 } ).limit( Number(limit) ).toArray(function(err, result){
       if(err) {
         res.status(500).json(err)
       } else {
